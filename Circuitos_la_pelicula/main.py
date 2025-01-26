feb6bdf18e8a3b1fc50059fbce478da7f1c48fbb
Cooldude0001 <167359375+Cooldude0001@users.noreply.github.com>
2025-01-25 22:31:15 -0500

    Add files via upload
    - Se añadio la clase circuito, como demostración no como versión completa.

--- Circuitos_la_pelicula/main.py
+++ Circuitos_la_pelicula/main.py
@@ -4,14 +4,14 @@ from Model.Components.Inductor import Inductor
 from Model.Components.Condensador import Capacitor
 from Model.Components.Fuente import Fuente
 
+from Model.Circuit.Circuito import Circuito
+
 if __name__ == "__main__":
     pana_1 = Resistencia(300, "R1")
     pana_2 = Inductor(650, "H1")
     pana_3 = Capacitor(100, "C1")
     pana_4 = Fuente(5, "Fuente", "DC")
-    
-    
-    pana_1.cambiar_valor(15)
-    print(f" valor de {pana_1._nombre} cambiado a: {pana_1._valor}")
-    
-    # Funciona :]
+
+    # Prueba
+    circuito = Circuito("Circuito 1", [pana_1, pana_2, pana_3, pana_4], "Serie")
+    print(circuito.corriente)
